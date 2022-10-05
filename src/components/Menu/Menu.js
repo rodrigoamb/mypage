964d11e8c537c08457620a6620e26bdbf0e71e38
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2022-10-05 19:33:48 -0300

    iniciando menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { ContainerMenu } from "./MenuStyles";
-import { NavLink } from "react-router-dom";
+import React from 'react';
+import { ContainerMenu } from './MenuStyles';
+import { NavLink } from 'react-router-dom';
 
-import { IoMdClose } from "react-icons/io";
+import { IoMdClose } from 'react-icons/io';
 
 const Menu = () => {
   return (
     <ContainerMenu>
-      <ul className="content-ul">
+      <ul className='content-ul'>
         <li>
-          <NavLink className="btn-nav" to={"/"} end>
+          <NavLink className='btn-nav' to={'/'} end>
             Sobre mim
           </NavLink>
         </li>
         <li>
-          <NavLink className="btn-nav" to={"/portfolio"}>
+          <NavLink className='btn-nav' to={'/portfolio'}>
             Portfólio
           </NavLink>
         </li>
         <li>
-          <NavLink className="btn-nav" to={"/contato"}>
+          <NavLink className='btn-nav' to={'/contato'}>
             Contato
           </NavLink>
         </li>
       </ul>
-      <IoMdClose className="icon-close" />
+      <IoMdClose className='icon-close' />
     </ContainerMenu>
   );
 };
--- src/components/Menu/MenuStyles.js
+++ src/components/Menu/MenuStyles.js
@@ -1,17 +1,50 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const ContainerMenu = styled.div`
-  background-color: red;
+  font-size: 1.2rem;
+  background: rgb(204, 70, 252);
+  background: linear-gradient(
+    0deg,
+    rgba(204, 70, 252, 0.85) 0%,
+    rgba(30, 50, 154, 0.8463760504201681) 100%
+  );
+  backdrop-filter: blur(8px);
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 100%;
-  position: absolute;
   top: 0;
   bottom: 0;
   right: 0;
   left: 0;
   z-index: 10;
+  position: absolute;
+
+  .icon-close {
+    color: #eee;
+    position: absolute;
+    top: 20px;
+    right: 20px;
+    font-size: 3rem;
+  }
+
+  .content-ul {
+    list-style: none;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    li {
+      margin-bottom: 30px;
+    }
+
+    a {
+      text-transform: uppercase;
+      text-decoration: none;
+      font-weight: bold;
+      color: #eee;
+    }
+  }
 `;
