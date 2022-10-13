f4a854e946da5dadc3e039c52ac9e7744b0d7498
Rodrigo Medeiros <dev.rodrigomedeiros@gmail.com>
2022-10-13 14:50:24 -0300

    adicionando depoimento 2

--- src/components/CardDepositions/addDepositions.js
+++ src/components/CardDepositions/addDepositions.js
@@ -1,30 +1,31 @@
-import photo1 from '../../assets/peoples/people1.jpeg';
+import photo1 from "../../assets/peoples/people1.jpeg";
+import photo2 from "../../assets/peoples/people2.png";
 
 export const DepositionsList = [
   {
     id: 1,
     photo: photo1,
-    deponent: 'Carlos José',
-    profession: 'Dev Sênior Front-End',
+    deponent: "Carlos José",
+    profession: "Dev Sênior Front-End",
     deposition:
       '"Rodrigo é meu aluno e tem sido um dos maiores destaques da turma, sempre fazendo todas as entregas com excelência, nunca só no básico mas sim sempre no excelente. No aspecto pessoal demostra ter uma convivência muito agradável e espirito colaborativo!"',
-    linkedin: 'https://www.linkedin.com/in/carlosxjm/',
+    linkedin: "https://www.linkedin.com/in/carlosxjm/",
   },
   {
     id: 2,
-    photo:
-      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png',
-    deponent: 'Nome',
-    profession: 'cargo',
+    photo: photo2,
+    deponent: "Rodolpho Pinheiro",
+    profession: "Dev Front-End Jr.",
     deposition:
-      '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores labore id tenetur pariatur sequi repudiandae consequatur magni. Vel hic consectetur quis error eligendi itaque dignissimos iure dicta id repellendus."',
+      '"O Rodrigo desde quando começamos a estudar percebi logo um grande interesse em aprender, muito esforçado, prestativo e sem dúvidas um grande profissional,  passou a ser um grande amigo e tenho aprendido muito com ele."',
+    linkedin: "https://www.linkedin.com/in/rodolphopinheiro/",
   },
   {
     id: 3,
     photo:
-      'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png',
-    deponent: 'Nome',
-    profession: 'Cargo',
+      "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png",
+    deponent: "Nome",
+    profession: "Cargo",
     deposition:
       '"Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem maiores labore id tenetur pariatur sequi repudiandae consequatur magni. Vel hic consectetur quis error eligendi itaque dignissimos iure dicta id repellendus."',
   },
